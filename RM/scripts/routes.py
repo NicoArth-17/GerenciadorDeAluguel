99421f68beb4730341c9e4a0e736809680c312b3
NicoArth-17 <nicolarthur17@hotmail.com>
2024-04-09 22:32:44 -0300

    construçao navbar

--- RM/scripts/routes.py
+++ RM/scripts/routes.py
@@ -8,3 +8,23 @@ def login():
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+@app.route('/AdcProduto')
+def adcproduto():
+    return render_template('adcproduto.html')
+
+@app.route('/alugueis')
+def alugueis():
+    return render_template('alugueis.html')
+
+@app.route('/clientes')
+def clientes():
+    return render_template('clientes.html')
+
+app.route('/calendario')
+def calendario():
+    return render_template('calendario.html')
+
+app.route('/caixa')
+def caixa():
+    return render_template('caixa.html')
